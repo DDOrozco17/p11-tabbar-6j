79eed79e25af838eb037d2eaf4fc7ad897b0d7e4
DIEGO_DIAZ_OROZCO <a.21308051280460@cbtis128.edu.mx>
2024-02-29 17:00:26 +0000

    actualizado

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,81 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(AppTabBar());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class AppTabBar extends StatelessWidget {
+  const AppTabBar({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: true,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      debugShowCheckedModeBanner: false,
+      title: "Ejemplo TabBar",
+      theme: ThemeData(primarySwatch: Colors.teal),
+      home: MiPaginaInicial(),
     );
   }
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class MiPaginaInicial extends StatefulWidget {
+  const MiPaginaInicial({Key? key}) : super(key: key);
 
   @override
+  State<MiPaginaInicial> createState() => _MiPaginaInicialState();
+}
+
+class _MiPaginaInicialState extends State<MiPaginaInicial> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("TabBar Diego Diaz"),
+          centerTitle: true,
+          bottom: const TabBar(tabs: [
+            Tab(
+              text: "Pinturas",
+              icon: Icon(Icons.ac_unit),
+            ),
+            Tab(
+              text: "Mochilas",
+              icon: Icon(Icons.payment),
+            ),
+            Tab(
+              text: "Papel",
+              icon: Icon(Icons.accessibility_new_outlined),
+            ),
+            Tab(
+              text: "Cartas",
+              icon: Icon(Icons.add_home_work_sharp),
+            ),
+          ] //TextoIcono
+              ),
         ),
+        body: TabBarView(children: const <Widget>[
+          Center(
+              child: Text(
+            "Tu",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+          )),
+          Center(
+            child: Text(
+              "Mama",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+            ),
+          ),
+          Center(
+              child: Text(
+            "Me",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+          )),
+          Center(
+              child: Text(
+            "Gusta",
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+          )),
+        ]),
       ),
     );
-  }
+  } //widgets
 }
